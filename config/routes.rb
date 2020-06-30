f060f7670f85d466e4b42aedca0e81fff6160085
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-30 16:20:24 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # config/routes.rb
- 
+
   get 'authors/:id/posts'
   get 'authors/:id/posts/:post_id'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
