084acc107842fa98d8ac2370b56950d57c9f68a8
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-30 16:53:00 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
     end
   end
   #the above is using a conditional to tell us whether we are trying to access a post by a certain author or all posts
-  #params[:author_id] comes from our nested routes. Rails automatically takes the parent name and appends and _id it it 
+  #params[:author_id] comes from our nested routes. Rails automatically takes the parent name and appends and _id to it 
 
   def show
     @post = Post.find(params[:id])
